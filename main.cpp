6f609ce78e5a502675bef8703386a8eeaaef6391
Gonzalo <gonzalo.alfacaso@gmail.com>
2019-08-22 17:42:40 -0500

    todo menos 19-20

--- main.cpp
+++ main.cpp
@@ -8,3 +8,79 @@ void ejercicio_x (int, int) {
 int main (int, char * []){
     return 1;
 }
+/*
+ejercicio 1
+1 2 3 4 5 6 7 8 9 10
+
+ejercicio 2
+7 5 10
+
+ejercicio 3
+T
+L
+T
+
+ejercicio 4
+opción 4
+
+ejercicio 5
+da error ya que el tipo de dato del puntero P debe ser igual al tipo de dato de la variable a la que esta apuntando (la cual es char, mas no double).
+
+ejercicio 6
+void swap(int *x, int *y)  
+	 {  
+    int temp = *x;  
+    *x = *y;  
+    *y = temp;  
+	 }
+
+ejercicio 7
+2 8
+
+ejercicio 8
+0
+8
+
+ejercicio 9
+8
+15
+
+ejercicio 10
+8
+
+ejercicio 11
+ int* a = new int;
+ int* ptr = &*a;
+ std::cout << *a << &a << &ptr;
+
+ejercicio 12
+13 13 13
+
+ejercicio 13
+7 5 10
+
+ejercicio 14
+4 1
+
+ejercicio 15
+	int suma(int* i, int* j)
+	{
+		int* r = new int;
+		*r = *i + *j;
+		return *r;
+	}
+
+ejercicio 16
+19
+
+ejercicio 17
+imprimiria un error por no darle un valor al puntero despues de declararlo
+
+ejercicio 18
+18
+
+ejercicio 19
+	
+
+*/
+
